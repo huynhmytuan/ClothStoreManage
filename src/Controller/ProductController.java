748232eeeec402816f5d52c2da518d8a8d52e3e2
Shino <Shino@10.10.137.106>
2021-01-08 11:29:17 +0700

    Long

--- src/Controller/ProductController.java
+++ src/Controller/ProductController.java
@@ -1,20 +1,29 @@
 package Controller;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXTextField;
+
+import Model.*;
+import Model.ProductUtil;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 
-public class ProductController {
+public class ProductController implements Initializable {
+	ProductUtil pu = new ProductUtil();
 
     @FXML
     private AnchorPane productUI;
 
     @FXML
-    private TableView<product> table_product;
+    private TableView<Product> table_product;
 
     @FXML
     private TableColumn<?, ?> col_id;
@@ -71,5 +80,11 @@ public class ProductController {
     void btn_search_Clicked(MouseEvent event) {
 
     }
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
