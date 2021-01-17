af2bd9ecaef6106228dceb90d35875c96f01bf00
Admin <Admin@Admin-PC>
2021-01-17 10:58:55 +0700

    nhan commit

--- src/application/Main.java
+++ src/application/Main.java
@@ -16,7 +16,6 @@ public class Main extends Application {
 	public void start(Stage stage) {
 		try {
 			Parent root = FXMLLoader.load(getClass().getResource("../View/LoginUI.fxml"));
-			
 			Scene scene = new Scene(root);
 			stage.initStyle(StageStyle.UNDECORATED);
 			stage.initStyle(StageStyle.TRANSPARENT);
